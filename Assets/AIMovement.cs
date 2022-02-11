035f1549e42c6a9c93c546484af8a890eb6a8658
CapelaGames <andrew@Capelagames.com.au>
2022-02-11 13:30:57 +1100

    Made AI move to a position

--- Assets/AIMovement.cs
+++ Assets/AIMovement.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //create variable
+    //place data in variable
+    //use variable
+    public GameObject position0;
+    public GameObject position1;
+    
+    
+    private void Update()
     {
+        Vector2 AIPosition = transform.position;
         
+        /*transform.position = Vector2.MoveTowards(transform.position, 
+                                            position0.transform.position,
+                                                         Time.deltaTime);*/
+
+        //method 1
+        /*if(transform.position.x < position0.transform.position.x)
+        {
+            //Move right
+            AIPosition.x += (1 * Time.deltaTime);
+            transform.position = AIPosition;
+        }
+        else
+        {
+            //Move Left
+            AIPosition.x -= (1 * Time.deltaTime);
+            transform.position = AIPosition;
+        }
+
+        //method 2
+        if (transform.position.y < position0.transform.position.y)
+        {
+            transform.position += (Vector3) Vector2.up * 1 * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= (Vector3) Vector2.up * 1 * Time.deltaTime;
+        }*/
+        
+        //direction from A to B
+        // is B - A
+
+        //method 3
+        Vector2 directionToPos0 = (position0.transform.position - transform.position);
+        directionToPos0.Normalize();
+        transform.position += (Vector3) directionToPos0 * 1 * Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
